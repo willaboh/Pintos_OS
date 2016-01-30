8b7caad91f3fdd01e3e84b40acedf6f77037b5e6
willaboh <willaboh@gmail.com>
2016-01-30 03:07:03 +0000

    Inserts thread into ready list in order of priority

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -250,7 +250,10 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered(&ready_list,
+                      &t->elem,
+                      &compare_priority,
+                      NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -321,7 +324,10 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered(&ready_list,
+                        &cur->elem,
+                        &compare_priority,
+                        NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
